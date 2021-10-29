66640daf114163e5cc623081c7d82ae808c47d92
mohan Gautam <rajesh.gautam010@gmail.com>
2021-10-29 18:19:41 +0200

    Bare-bone skeleton added

---
+++ app/src/main/java/com/example/encryption/Encrypt.java
@@ -0,0 +1,16 @@
+package com.example.encryption;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Encrypt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encrypt);
+
+
+
+    }  //-------------------------------------- End of OnCreate----------------------------
+}
--- app/src/main/java/com/example/encryption/MainActivity.java
+++ app/src/main/java/com/example/encryption/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.encryption;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button Btn_encrypt, Btn_decrypt;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        Btn_encrypt = findViewById(R.id.Btn_encrypt);
+        Btn_decrypt = findViewById(R.id.Btn_decrypt);
+
+        Btn_encrypt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Encrypt.class);
+                startActivity(i);
+            }
+        });
+
+
+        Btn_decrypt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Decrypt.class);
+                startActivity(i);
+            }
+        });
+
+
+    } //end of on create function
 }
